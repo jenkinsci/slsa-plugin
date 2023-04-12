be4e4bf59a45a68e22cced9c266ed2e5341fc83a
Thomas Neidhart <thomas.neidhart@gmail.com>
2023-04-12 19:24:23 +0200

    Update pom.xml based on feedback from Jenkins Hosting Checker.

--- src/main/java/org/jenkinsci/plugins/slsa/ProvenanceAction.java
+++ src/main/java/org/jenkinsci/plugins/slsa/ProvenanceAction.java
@@ -39,7 +39,7 @@ public class ProvenanceAction implements RunAction2 {
 
     @Override
     public String getIconFileName() {
-        return "/plugin/slsa-provenance/icons/slsa-logo.png";
+        return "/plugin/slsa/icons/slsa-logo.png";
     }
 
     @Override
